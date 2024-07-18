3a2c48a2d0ff85614fce68b45633229c1f88ec80
Tristan Lino <tristanlino@Tristans-MacBook-Pro.local>
2024-07-18 11:27:18 -0600

    feat: add documentation

--- REST_API/REST_API/settings.py
+++ REST_API/REST_API/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Own
     'rest_framework',
-    'post'
+    'post',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- REST_API/REST_API/urls.py
+++ REST_API/REST_API/urls.py
@@ -17,8 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from post.API.router import router_posts
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="REST API",
+      default_version='v1',
+      description="Rest Api with Django and rest framework django",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="tristan.a.lino@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   # permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router_posts.urls))
+    path('api/', include(router_posts.urls)),
+    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
